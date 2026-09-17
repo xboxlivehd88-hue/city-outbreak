914962c3330587dc6ae2dc92d95886bd1df3c4f9
xboxlivehd88-hue <xboxlivehd88@gmail.com>
2026-09-17 11:38:26 -0400

    Add central game configuration

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const GAME_CONFIG = Object.freeze({
+  maxHealth: 100,
+  walkSpeed: 5,
+  sprintSpeed: 8,
+  corpseLifetimeSeconds: 18,
+  grenadeFuseSeconds: 2.15,
+  grenadeRadius: 7.5,
+  startingGrenades: 2,
+  assetRoot: './assets',
+});
